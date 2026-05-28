8c304c551167a4d15c2103e785b3d8169644ad11
Miguel Ramos <return.infox@gmail.com>
2026-05-28 02:23:38 -0400

    fix: add eslint.config.js for ESLint 9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+﻿const tseslint = require('typescript-eslint');
+const eslintPluginPrettier = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    extends: [
+      ...tseslint.configs.recommended,
+    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+    },
+  },
+  eslintPluginPrettier,
+);
